753c4a179d56191ea80d300a6231b193f2c9003c
Juan Marcos <juanmarcoscabezas@gmail.com>
2020-08-14 23:18:18 -0500

    Fixing bug

--- hand.js
+++ hand.js
@@ -44,7 +44,7 @@ const context = canvas.getContext("2d");
 
 let isVideo = false;
 let model = null;
-let videoInterval = 0;
+let videoInterval = 10;
 
 const modelParams = {
     flipHorizontal: true, // flip e.g for video  
@@ -52,6 +52,14 @@ const modelParams = {
     iouThreshold: 0.5, // ioU threshold for non-max suppression
     scoreThreshold: 0.6, // confidence threshold for predictions.
 }
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+    // detect objects in the image.
+    model = lmodel
+    console.log("Loaded Model!");
+});
+
 
 function startVideo() {
     handTrack.startVideo(video).then(function (status) {
@@ -79,33 +87,34 @@ function toggleVideo() {
 }
 
 function runDetection() {
-    model.detect(video).then(predictions => {
-        // console.log("Predictions: ", predictions);
-        // get the middle x value of the bounding box and map to paddle location
-        model.renderPredictions(predictions, canvas, context, video);
-        if (predictions[0]) {
-            let x = predictions[0].bbox[0];
-            let y = predictions[0].bbox[1];
-            let w = predictions[0].bbox[2];
-            let h = predictions[0].bbox[3];
-            // let midval = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2)
-            // gamex = document.body.clientWidth * (midval / video.width)
-            apple.checkCollision(x, y, w, h);
-        }
+    if (model) {
+        model.detect(video).then(predictions => {
+            // console.log("Predictions: ", predictions);
+            // get the middle x value of the bounding box and map to paddle location
+            model.renderPredictions(predictions, canvas, context, video);
+            if (predictions[0]) {
+                let x = predictions[0].bbox[0];
+                let y = predictions[0].bbox[1];
+                let w = predictions[0].bbox[2];
+                let h = predictions[0].bbox[3];
+                // let midval = predictions[0].bbox[0] + (predictions[0].bbox[2] / 2)
+                // gamex = document.body.clientWidth * (midval / video.width)
+                apple.checkCollision(x, y, w, h);
+            }
+            if (isVideo) {
+                setTimeout(() => {
+                    runDetection(video)
+                }, videoInterval);
+            }
+        });
+    } else {
         if (isVideo) {
             setTimeout(() => {
                 runDetection(video)
             }, videoInterval);
         }
-    });
+    }
 }
-
-// Load the model.
-handTrack.load(modelParams).then(lmodel => {
-    // detect objects in the image.
-    model = lmodel
-    console.log("Loaded Model!");
-});
 
 
 windowHeight = window.innerHeight;
